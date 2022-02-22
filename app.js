a415c8d22ceaa6b1e1d60054c2a8543183c0b4a4
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2022-02-22 23:37:13 +0100

    Création des fonctions de somme des points

---
+++ app.js
@@ -0,0 +1,30 @@
+let p1Score = 0;
+let p2Score = 0;
+
+let winningScore = 5;
+let isGameOver = false;
+
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+
+p1Button.addEventListener('click', function() {
+    if (!isGameOver) {
+        p1Score += 1;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+        }
+        p1Display.innerText = p1Score;
+    }
+})
+
+p2Button.addEventListener('click', function() {
+    if (!isGameOver) {
+        p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+        }
+        p2Display.innerText = p2Score;
+    }
+})
