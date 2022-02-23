16a6327a41158996d775789a630ec46b9766f73b
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2022-02-23 14:34:01 +0100

    Ajout des styles

--- app.js
+++ app.js
@@ -12,30 +12,34 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const resetButton = document.querySelector("#resetButton");
 
-winningScoreSelect.addEventListener('change', function() {
+winningScoreSelect.addEventListener('change', function () {
     winningScore = parseInt(this.value);
-    resetScore(); 
+    resetScore();
 })
 
-p1Button.addEventListener('click', function() {
+p1Button.addEventListener('click', function () {
     if (!isGameOver) {
         p1Score += 1;
         if (p1Score === winningScore) {
-            p1Display.classList.add('winner')
-            p2Display.classList.add('looser')
+            p1Display.classList.add('has-text-success')
+            p2Display.classList.add('has-text-danger')
             isGameOver = true;
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.innerText = p1Score;
     }
 })
 
-p2Button.addEventListener('click', function() {
+p2Button.addEventListener('click', function () {
     if (!isGameOver) {
         p2Score += 1;
         if (p2Score === winningScore) {
-            p2Display.classList.add('winner')
-            p1Display.classList.add('looser')
+            p2Display.classList.add('has-text-success')
+            p1Display.classList.add('has-text-danger')
             isGameOver = true;
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.innerText = p2Score;
     }
@@ -44,7 +48,7 @@ p2Button.addEventListener('click', function() {
 resetButton.addEventListener('click', resetScore)
 
 /**
- * Cette fonction permet de réinitialiser les résultats
+ * Cette fonction permet de réinitialiser les résultats et les styles des scores
  */
 function resetScore() {
     isGameOver = false;
@@ -52,6 +56,8 @@ function resetScore() {
     p2Score = 0;
     p1Display.textContent = '0';
     p2Display.textContent = '0';
-    p1Display.classList.remove('winner', 'looser');
-    p2Display.classList.remove('winner', 'looser');
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
